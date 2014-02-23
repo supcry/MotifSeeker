3438903beb41a403c622633635e2badb28baec5f
supcry <supcry@gmail.com>
2014-02-24 00:01:07 +0400

    Добавил StaticDictionary с тестами.

---
+++ Sources/MotifSeeker.Test/StaticDictionaryTest.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using MotifSeeker.Helpers;
+using NUnit.Framework;
+
+namespace MotifSeeker.Test
+{
+	[TestFixture]
+    public class StaticDictionaryTest
+    {
+		[Test]
+		public void _01_Bools()
+		{
+		    var dic =
+		        new StaticDictionary<bool, bool>(new[]
+		        {new KeyValuePair<bool, bool>(false, true), new KeyValuePair<bool, bool>(true, false)});
+            Assert.AreEqual(2, dic.Count);
+			Assert.AreEqual(false, dic[true]);
+            Assert.AreEqual(true, dic[false]);
+		}
+
+        [Test]
+        public void _02_SmallInts()
+        {
+            var rnd = new Random(1);
+            var pairs = new[]
+            {
+                new KeyValuePair<int, int>(rnd.Next(), rnd.Next()),
+                new KeyValuePair<int, int>(rnd.Next(), rnd.Next()),
+                new KeyValuePair<int, int>(rnd.Next(), rnd.Next())
+            };
+
+            TestDictionary(pairs);
+            TestStaticDictionary(pairs);
+        }
+
+        [Test]
+        public void _03_Ints()
+        {
+            var rnd = new Random(1);
+            const int count = 1000000;
+            var hs = new HashSet<int>();
+            var pairs = Enumerable.Repeat(1, int.MaxValue)
+                                  .Select(_ => new KeyValuePair<int, int>(rnd.Next(), rnd.Next()))
+                                  .Where(p => hs.Add(p.Key))
+                                  .Take(count)
+                                  .ToArray();
+
+            TestDictionary(pairs);
+            TestStaticDictionary(pairs);
+        }
+
+	    void TestDictionary(ICollection<KeyValuePair<int, int>> data)
+	    {
+	        var sw = new Stopwatch();
+            sw.Start();
+	        var dic = new Dictionary<int, int>(data.Count);
+	        foreach (var d in data)
+	            dic[d.Key] = d.Value;
+            sw.Stop();
+            var sw2 = new Stopwatch();
+            sw2.Start();
+	        foreach (var d in data.Reverse())
+	            Assert.AreEqual(d.Value, dic[d.Key]);
+            sw2.Stop();
+            Console.WriteLine("Dictionary: creation=" + sw.Elapsed + ", search=" + sw2.Elapsed);
+	    }
+
+        void TestStaticDictionary(ICollection<KeyValuePair<int, int>> data)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var dic = new StaticDictionary<int, int>(data);
+            sw.Stop();
+            Assert.AreEqual(data.Count, dic.Count);
+            var sw2 = new Stopwatch();
+            sw2.Start();
+            foreach (var d in data)
+                Assert.AreEqual(d.Value, dic[d.Key]);
+            sw2.Stop();
+            Console.WriteLine("StaticDictionary: creation=" + sw.Elapsed + ", search=" + sw2.Elapsed);
+        }
+
+    }
+}
---
+++ Sources/MotifSeeker/Helpers/StaticDictionary.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace MotifSeeker.Helpers
+{
+    /// <summary>
+    /// Реализация быстрого словаря без возможности вставки/удаления, но с минимальным расходом по памяти (не более 2х int'ов на пару key-value).
+    /// </summary>
+    /// <typeparam name="TKey">Ключ пары. Хэш ключа должен быть достаточно хорош.</typeparam>
+    /// <typeparam name="TValue">Значение пары.</typeparam>
+    public sealed class StaticDictionary<TKey, TValue> : IDictionary<TKey, TValue>
+    {
+        private readonly int _ofsBitsCount;
+        private readonly int[] _ofs;       // таблица смещений в массиве хэшей
+        private readonly uint[] _hashes;    // хэши ключей (отсортированы по возрастанию)
+        private readonly TKey[] _keys;     // ключи (сортировка соответствует _hashes)
+        private readonly TValue[] _values; // значения (сортировка соответствует _keys)
+
+        public StaticDictionary(ICollection<KeyValuePair<TKey, TValue>> pairs)
+        {
+            // Пока предполагаем, что дубликатов нет.
+
+            _hashes = new uint[pairs.Count];
+            _keys = new TKey[pairs.Count];
+            _values = new TValue[pairs.Count];
+            int i = 0;
+            _ofsBitsCount = GetOfsBitsCount(pairs.Count);
+            _ofs = new int[(int)Math.Round(Math.Pow(2, _ofsBitsCount))];
+            int lastOfs = 0;
+            //int lastOfsPos = -1;
+            foreach (var t in pairs.Select(p => new Tuple<uint, TKey, TValue>((uint)p.Key.GetHashCode(), p.Key, p.Value)).OrderBy(p => p.Item1))
+            {
+                _hashes[i] = t.Item1;
+                _keys[i] = t.Item2;
+                _values[i] = t.Item3;
+                
+                var ofs = (int)(t.Item1 >> (sizeof(uint) * 8 - _ofsBitsCount));
+                if (lastOfs != ofs)
+                {
+                    for (int j = lastOfs + 1; j <= ofs; j++)
+                        _ofs[j] = i;
+                    lastOfs = ofs;
+                }
+                i++;
+            }
+            for (int j = lastOfs + 1; j < _ofs.Length; j++)
+                _ofs[j] = -1;
+        }
+
+        private int GetKeyId(TKey key)
+        {
+            var hash = (uint)key.GetHashCode();
+            var ofs = (int)(hash >> (sizeof(uint) * 8 - _ofsBitsCount));
+            int id = _ofs[ofs];
+            if (id == -1)
+                return -1;
+            while (_hashes[id] < hash)
+                id++;
+            while (_hashes[id] == hash)
+            {
+                if (_keys[id].Equals(key))
+                    return id;
+                id++;
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// Сколько бит будет задействовано при работе с таблицей _ofs
+        /// </summary>
+        /// <param name="dicSize"></param>
+        /// <returns></returns>
+        private static int GetOfsBitsCount(int dicSize)
+        {
+            var part = dicSize;
+            int bitsCount = 0;
+            while ((1 << bitsCount) < part)
+                bitsCount++;
+            Debug.Assert(bitsCount > 0);
+            return bitsCount;
+        }
+
+
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            return GetEnumerable().GetEnumerator();
+        }
+
+        public IEnumerable<KeyValuePair<TKey, TValue>> GetEnumerable()
+        {
+            return _keys.Select((t, i) => new KeyValuePair<TKey, TValue>(t, _values[i]));
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotSupportedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotSupportedException();
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            TValue ret;
+            if (TryGetValue(item.Key, out ret))
+                return item.Equals(ret);
+            return false;
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            throw new NotSupportedException();
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotSupportedException();
+        }
+
+        public int Count { get { return _keys.Length; } }
+        public bool IsReadOnly { get { return true; } }
+        public bool ContainsKey(TKey key)
+        {
+            return GetKeyId(key) != -1;
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            throw new NotSupportedException();
+        }
+
+        public bool Remove(TKey key)
+        {
+            throw new NotSupportedException();
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            var id = GetKeyId(key);
+            if (id == -1)
+            {
+                value = default(TValue);
+                return false;
+            }
+            value = _values[id];
+            return true;
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                TValue ret;
+                if (TryGetValue(key, out ret))
+                    return ret;
+                throw new KeyNotFoundException();
+            }
+            set { throw new NotSupportedException(); }
+        }
+
+        public ICollection<TKey> Keys { get { return _keys; } }
+        public ICollection<TValue> Values { get { return _values; } }
+    }
+}
